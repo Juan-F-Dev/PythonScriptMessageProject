14bda532941668a72192afeaffad034f2cbd2559
Juan-F-Dev <farismendi27.dev@gmail.com>
2025-02-23 00:50:49 -0500

    Update recordatorio.py

--- recordatorio.py
+++ recordatorio.py
@@ -5,11 +5,6 @@ import schedule
 import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from dotenv import load_dotenv 
-
-if os.path.exists('.env'):
-    from dotenv import load_dotenv
-    load_dotenv()
 
 def send_email():
     """Función que envía el correo utilizando las variables de entorno."""
